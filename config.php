5d38b94ca7243ca2177c6b1045dcc03197d58326
Andrew <brazaandrew@gmail.com>
2025-10-20 00:39:24 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,4 @@
 <?php
-$host = "portfoliodb.cpkc6sc8if8f.ap-southeast-2.rds.amazonaws.com";
-$username = "admin";  // or your RDS master username
-$password = "Law_08199823";  // your RDS password
-$dbname = "portfoliodb";
-
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-?>
-
-<?php
 $servername = "portfoliodb.cpkc6sc8if8f.ap-southeast-2.rds.amazonaws.com";
 $username = "admin";
 $password = "Law_08199823";
@@ -26,3 +11,4 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 ?>
+
