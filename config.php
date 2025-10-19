4c4b64e7d0988ee808777de1549c3f6f731cfcd6
Andrew <brazaandrew@gmail.com>
2025-10-20 01:02:22 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,13 +4,13 @@ $username = "admin";
 $password = "Law_08199823";
 $dbname = "portfoliodb";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
+
 echo "Connected successfully";
 ?>
-
-
-
