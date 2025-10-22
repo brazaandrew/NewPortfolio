8c38e47dffcab1526e3b0e52f5bbecd59673c452
Andrew <brazaandrew@gmail.com>
2025-10-22 23:05:03 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $servername = "personalportfolio.cpkc6sc8if8f.ap-southeast-2.rds.amazonaws.com";
 $username = "admin";
 $password = "C1sc012345";
@@ -14,6 +17,7 @@ if ($conn->connect_error) {
 }
 echo "✅ Connected successfully to RDS database!";
 ?>
+
 
 
 
