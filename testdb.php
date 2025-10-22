6fee2c6d96154b222536d74343cee120001ee3b2
Andrew <brazaandrew@gmail.com>
2025-10-22 21:54:24 +0800

    Update testdb.php

--- testdb.php
+++ testdb.php
@@ -4,14 +4,10 @@ $username = "admin";
 $password = "C1sc012345";
 $dbname = "personalportfolio";
 
-
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-echo "Connected successfully";
+echo "Connected successfully to RDS!";
 ?>
