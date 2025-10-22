b8d23011e85090e9e6cdefcf51e66486bc065459
Andrew <brazaandrew@gmail.com>
2025-10-22 23:30:43 +0800

    Create s3-config.php

---
+++ s3-config.php
@@ -0,0 +1,18 @@
+<?php
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+
+// S3 configuration
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => 'ap-southeast-2', // Your bucket region
+    'credentials' => [
+        'key'    => 'AKIAX36PVLHDHVHI4ZMK',
+        'secret' => 'f3gDq512ysg6SJ7mMxR08P7O1L7WCzmDQf6gK4ps',
+    ]
+]);
+
+$bucket = 'portfolioandrew'; // your S3 bucket name
+?>
