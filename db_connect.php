9d7bd2e00189a06a9cb7e17eaaef21d67e8b511c
Andrew <brazaandrew@gmail.com>
2025-10-22 22:55:57 +0800

    Add database connection script for RDS

---
+++ db_connect.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "personalportfolio.cpkc6sc8if8f.ap-southeast-2.rds.amazonaws.com";
+$username = "admin";
+$password = "C1sc012345";
+$dbname = "personalportfolio";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "✅ Connected successfully to RDS database!";
+?>
