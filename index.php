67987b306b469d0666b6c74385f6c8e04cf1cb82
Andrew <brazaandrew@gmail.com>
2025-10-23 00:38:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -536,21 +536,22 @@
                     <div class="contact-item">https://github.com/brazaandrew</div>
                 </div>
                 
-               <form class="contact-form" method="POST" action="process_contact.php">
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" id="name" name="name" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" id="email" name="email" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="message">Message</label>
-                        <textarea id="message" name="message" required></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Send Message</button>
-                </form>
+             <form class="contact-form" method="POST" action="process_contact.php">
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" required>
+    </div>
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" required>
+    </div>
+    <div class="form-group">
+        <label for="message">Message</label>
+        <textarea id="message" name="message" required></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Send Message</button>
+</form>
+
             </div>
         </div>
     </section>
@@ -616,3 +617,4 @@
 </body>
 
 </html>
+
